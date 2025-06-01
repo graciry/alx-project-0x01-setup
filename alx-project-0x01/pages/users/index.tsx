f8445b0a17a9008975ac1df47e1dfc8b7d994076
graciry <ngunyigwothaya@gmail.com>
2025-06-01 12:45:58 +0300

    Task 3

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,23 +1,24 @@
+// pages/users/index.tsx
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
-  posts: UserProps[];
+  users: UserProps[];
 }
 
-const Users: React.FC<UsersPageProps> = ({ posts }) => {
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-4 overflow-y-auto">
-        <div className="flex justify-between mb-4">
-          <h1 className="text-2xl font-semibold">User Directory</h1>
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">User List</h1>
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
-        <div className="grid grid-cols-2 gap-4">
-          {posts?.map((user: UserProps) => (
-            <UserCard key={user.id} {...user} />
+        <div className="grid grid-cols-3 gap-4">
+          {users.map((user, index) => (
+            <UserCard key={index} {...user} />
           ))}
         </div>
       </main>
@@ -27,11 +28,11 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const users = await response.json();
 
   return {
     props: {
-      posts,
+      users,
     },
   };
 }
