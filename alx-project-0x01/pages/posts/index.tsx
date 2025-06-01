59f26bc2e35259d15792ad49d1491742f336f524
graciry <ngunyigwothaya@gmail.com>
2025-06-01 12:50:26 +0300

    Task 3 modified

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -12,31 +12,35 @@ const Posts: React.FC<PostsPageProps> = ({ posts }) => {
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <div className="flex justify-between items-center mb-6">
+        <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">Post Content</h1>
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {
-            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
-              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
-            ))
-          }
+        <div className="grid grid-cols-3 gap-2">
+          {posts.map((post) => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              userId={post.userId}
+              id={post.id}
+            />
+          ))}
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
 
 export default Posts;
