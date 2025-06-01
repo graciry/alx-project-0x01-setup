965e04bad3f0cc348039268f4ad514f1b944bdef
graciry <ngunyigwothaya@gmail.com>
2025-06-01 12:26:03 +0300

    Task 1

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 
 const PostsPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Posts</h1>
-      <PostCard title="First Post" body="This is the body of the first post." />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-8 flex-grow">
+        <h1 className="text-3xl font-bold mb-4">Posts</h1>
+        <PostCard title="First Post" body="This is the body of the first post." />
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import Header from "@/components/layout/Header";
 import Button from "@/components/common/Button";
 
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Users</h1>
-      <Button title="Click Me" onClick={() => alert("Button clicked!")} />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-8 flex-grow">
+        <h1 className="text-3xl font-bold mb-4">Users</h1>
+        <Button title="Click Me" onClick={() => alert("Button clicked!")} />
+      </main>
     </div>
   );
 };
